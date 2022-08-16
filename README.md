9e1f5b1219e1a8eb96a7dc3e0240fdac657ffd72
Druzian <druziii@hotmail.com>
2022-08-16 17:25:36 -0300

    password generator finalizado

---
+++ README.md
@@ -0,0 +1,5 @@
+##Uma aplicação de geração de senhas aleatórias com limitadores para treino com React Native. 
+##Temas estudados: 
+* UseEffect
+* Expressões Regulares
+* Treinamento de estilização com CSS
---
+++ src/App.js
@@ -0,0 +1,15 @@
+import PassworGenerator from "./components/PassworGenerator";
+
+function App() {
+  return (
+    <main>
+        <div className="container">
+          <h1 className="app-title">Gerador de Senhas</h1>
+          
+          <PassworGenerator />
+        </div>
+    </main>
+  );
+}
+
+export default App;
